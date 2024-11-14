5ccdcb63968ef169d5a65c7ae57d54ff0995a752
Eman Nabi <f223626@cfd.nu.edu.pk>
2024-11-14 12:12:12 +0500

    Commit 2: Add more experimental changes

--- src/git_scd_lab/Login.java
+++ src/git_scd_lab/Login.java
@@ -3,8 +3,12 @@ package git_scd_lab;
 public class Login {
 
 	public boolean authenticate(String username, String password) {
-		System.out.println("Authenticating user...");
-		return username.equals("admin") && password.equals("password123");
-		}
+	    if (username == null || password == null) {
+	        System.out.println("Invalid input");
+	        return false;
+	    }
+	    System.out.println("Authenticating user...");
+	    return username.equals("admin") && password.equals("password123");
+	}
 }
 
